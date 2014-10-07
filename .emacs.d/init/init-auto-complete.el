dca2a2cf7d49238b5ea14d7beb9d003b8c7a21df
Anders Rønningen <anders@ronningen.priv.no>
2014-10-07 12:47:13 +0200

    Out with auto-complete.

--- .emacs.d/init/init-auto-complete.el
+++
@@ -1,5 +0,0 @@
-(message "*** Autocomplete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/dict")
-(ac-config-default)
-(provide 'init-auto-complete)
