b7f4651a564675031b1a1470cb408b7157f4b1ae
Anders Rønningen <anders@ronningen.priv.no>
2015-01-09 08:37:56 +0100

    No confirmation of smart-mode-line themes

--- .emacs.d/init/init-smart-mode-line.el
+++ .emacs.d/init/init-smart-mode-line.el
@@ -2,6 +2,7 @@
 (setq powerline-arrow-shape 'curve)
 (setq powerline-default-separator-dir '(right . left))
 ;; These two lines you really need.
+(setq sml/no-confirm-load-theme t)
 (setq sml/theme 'powerline)
 (sml/setup)
 
