57995222b460045e91b3617cea06e950ade7a0e1
Anders Rønningen <anders@ronningen.priv.no>
2015-02-10 09:12:23 +0100

    Solarized-dark again

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -247,7 +247,7 @@
        (around fix-terminal-init-screen first () activate)
        (terminal-init-xterm))))
 
-(load-theme 'twilight t)
+(load-theme 'solarized-dark t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
