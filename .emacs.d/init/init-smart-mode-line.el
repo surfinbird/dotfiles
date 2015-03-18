492c1a9fc6e6a2bcc0348012b24cf4115df31604
Anders Rønningen <anders@ronningen.priv.no>
2015-03-18 11:12:10 +0100

    Dark theme.

--- .emacs.d/init/init-smart-mode-line.el
+++ .emacs.d/init/init-smart-mode-line.el
@@ -1,6 +1,7 @@
 ;; These two lines are just examples
 (setq powerline-arrow-shape 'curve)
 (setq powerline-default-separator-dir '(right . left))
+(setq sml/theme 'dark)
 ;; These two lines you really need.
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
