f2ecda12b9db1552ec1f0f7b1bdf2a632b14b163
Anders Rønningen <anders@ronningen.priv.no>
2015-04-05 20:59:28 +0200

    Use only melpa-stable.

--- .emacs.d/init/init-package.el
+++ .emacs.d/init/init-package.el
@@ -3,7 +3,7 @@
 
 ;; Add gnu and melpa to package repos
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 
