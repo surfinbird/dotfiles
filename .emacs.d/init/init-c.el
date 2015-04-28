856bdb25823a3c70983b83a7c6d39dde07297a72
Anders Rønningen <anders@ronningen.priv.no>
2015-04-28 20:12:38 +0200

    google-c-style.

--- .emacs.d/init/init-c.el
+++ .emacs.d/init/init-c.el
@@ -1,10 +1,10 @@
-;; C syntax
-(setq c-default-style "k&r"
-      c-basic-offset 4
-      c-auto-newline nil
-      c-tab-always-indent t)
-(c-set-offset 'substatement-open 0)
-(setq c-font-lock-extra-types (quote ("\\sw+_t" "bool" "complex" "imaginary" "FILE" "lconv" "tm" "va_list" "jmp_buf" "Lisp_Object" "TXPACKET")))
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook
+      (lambda()
+            (subword-mode)
+            (google-set-c-style)
+            (google-make-newline-indent)
+            (setq c-basic-offset 4)))
 
 (defun linux-c-mode ()
   "C mode with adjusted defaults for use with the Linux kernel."
