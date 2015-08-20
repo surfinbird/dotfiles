fc0ed397f9a9924cc0ff454ea76cc5731c294e75
Anders Rønningen <anders@ronningen.priv.no>
2015-08-20 08:17:49 +0200

    Load elpy when python is loaded. Strip WS on save.

--- .emacs.d/init/init--python.el
+++ .emacs.d/init/init--python.el
@@ -3,13 +3,16 @@
 (use-package elpy
   :ensure t
   :bind (("S-<f8>" . python-compile))
+  :init
+  (with-eval-after-load 'python (elpy-enable))
+  (add-hook 'elpy-mode-hook
+            (lambda ()
+              (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)))
+  (remove-hook 'elpy-modules 'elpy-module-flymake)
   :config
-  (elpy-enable)
   (use-package pylint
     :config
-    :bind (("C-<f7>" . pylint))
-    )
-  (remove-hook 'elpy-modules 'elpy-module-flymake)    
+    :bind (("C-<f7>" . pylint)))
 )
 
 (anr78:provide)
