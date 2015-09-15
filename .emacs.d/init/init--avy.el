771546812c6d3a2a20f4f638e8726a6b9127c155
Anders Rønningen <anders@ronningen.priv.no>
2015-09-15 17:54:28 +0200

    emacs: add avy shortcuts

--- .emacs.d/init/init--avy.el
+++ .emacs.d/init/init--avy.el
@@ -2,7 +2,12 @@
 
 (use-package avy
   :ensure t
-  :bind (("C-ø" . avy-goto-word-or-subword-1))
+  :bind (("C-ø" . avy-goto-word-or-subword-1)
+         ("C-:" . avy-goto-char)
+         ("C-'" . avy-goto-char-2)
+         ("M-g f" . avy-goto-line)
+         ("M-g w" . avy-goto-word-1)
+         ("M-g e" . avy-goto-word-0))
   :config
   (avy-setup-default)
   )
