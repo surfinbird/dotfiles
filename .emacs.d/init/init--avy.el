a7caeb85168c6ec551ac88391335b079feeec7f9
Anders Rønningen <anders@ronningen.priv.no>
2015-09-18 09:53:13 +0200

    Emacs: Avy setup.

--- .emacs.d/init/init--avy.el
+++ .emacs.d/init/init--avy.el
@@ -3,11 +3,9 @@
 (use-package avy
   :ensure t
   :bind (("C-ø" . avy-goto-word-or-subword-1)
-         ("C-:" . avy-goto-char)
-         ("C-'" . avy-goto-char-2)
-         ("M-g f" . avy-goto-line)
-         ("M-g w" . avy-goto-word-1)
-         ("M-g e" . avy-goto-word-0))
+         ("C-:" . avy-goto-char-timer)
+         ("M-g l" . avy-goto-line)
+         ("M-g w" . avy-goto-word-1))
   :config
   (avy-setup-default)
   )
