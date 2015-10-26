83be46b474823556f06ebaf8dd089b0f3c7095fc
Anders Rønningen <anders@ronningen.priv.no>
2015-10-26 13:57:04 +0100

    Emacs: simplify C setup.

--- .emacs.d/init/init--c.el
+++ .emacs.d/init/init--c.el
@@ -8,18 +8,9 @@
             (google-make-newline-indent)
             (setq c-basic-offset 4)))
 
-(defun linux-c-mode ()
+(defun linux-c-mode-offset ()
   "C mode with adjusted defaults for use with the Linux kernel."
   (interactive)
-  (c-mode)
-  (c-set-style "K&R")
   (setq c-basic-offset 8))
-
-(defun knr-c-mode ()
-  "C mode as k&r would want it."
-  (interactive)
-  (c-mode)
-  (c-set-style "K&R")
-  (setq c-basic-offset 4))
 
 (anr78:provide)
