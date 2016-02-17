07a10c6a26cde87fb3c1ff36cdf4c17525035cc5
Anders Rønningen <anr@MacBook-Pro.local>
2016-02-17 20:32:06 +0100

    Add spacemacs config

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ function doIt() {
     stuff=(".aliases .emacs.d .scripts .vim .xmonad .zprezto \
         .ackrc .bash_profile .bashrc .conkyrc .functions \
         .gitconfig .rtorrent.rc .screenrc .tmux.conf .vimrc \
-        .Xmodmap .Xresources .Xresources.d .xsessionrc .xmobarrc")
+        .Xmodmap .Xresources .Xresources.d .xsessionrc .xmobarrc \
+        .spacemacs")
     
     for el in $stuff; do
         ln -fsv $(pwd)/$el ~
