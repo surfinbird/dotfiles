c629a275f741109d20e8477e6835a41b1bf96bba
Anders Ronningen <anders@ronningen.priv.no>
2016-04-06 14:45:46 +0200

    Revert "Remove font setup stuff. Too fragile."
    This reverts commit 654e1fe46abe914f70a362ae80e7db37769a05dc.

---
+++ .config/start-session/common.d/load-Xresources.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "== Running reset_resources =="
+reset-resources
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function doIt() {
     stuff=(".aliases .emacs.d .scripts .vim .xmonad .zprezto \
         .ackrc .bash_profile .bashrc .conkyrc .functions \
         .gitconfig .rtorrent.rc .screenrc .tmux.conf .vimrc \
-        .Xmodmap .Xresources .xsessionrc .xmobarrc \
+        .Xmodmap .Xresources .Xresources.d .xsessionrc .xmobarrc \
         .spacemacs")
     
     for el in $stuff; do
