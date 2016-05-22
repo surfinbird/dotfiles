955c7dd93dc27b12dd672a9694eeab8a2c600d3b
Anders Ronningen <anders@ronningen.priv.no>
2016-05-22 11:29:39 +0200

    Cleanup bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
-git submodule update --init --recursive
-function doIt() {
+
+update_repos() {
+    git pull origin master
+    git submodule update --init --recursive
+    
+    if [ ! -d ~/.emacs.d ]; then
+        git clone git@github.com:syl20bnr/spacemacs.git ~/.emacs.d
+    fi
+}
+
+create_symlinks() {
     stuff=(".aliases .scripts .vim .xmonad .zprezto \
         .ackrc .bash_profile .bashrc .conkyrc .functions \
         .gitconfig .rtorrent.rc .screenrc .tmux.conf .vimrc \
         .Xmodmap .Xresources .Xresources.d .xsessionrc .xmobarrc \
         .spacemacs .i3blocks.conf")
-    
+
     for el in $stuff; do
         ln -fsv $(pwd)/$el ~
     done
@@ -16,22 +24,21 @@ function doIt() {
     for el in $(ls .config); do
         ln -fsv $(pwd)/.config/$el ~/.config/$el
     done
-
-#    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-#        --exclude "README.md" --exclude "*.swp" --exclude ".gitmodules" \
-#        -av --no-perms . ~
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
-    fi
-fi
-unset doIt
 
-if [ ! -d ~/.emacs.d ]; then
-    git clone git@github.com:syl20bnr/spacemacs.git ~/.emacs.d
-fi
+case $1 in
+    "--force"|"-f")
+        :
+        ;;
+    *)
+        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            exit 1
+        fi
+        ;;
+esac
+
+update_repos
+create_symlinks
+
