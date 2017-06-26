848f4f84755d35d32677d165c66e02182a8336ce
Anders Rønningen <anders@ronningen.priv.no>
2017-06-26 22:36:06 +0200

    bootstrap: cosmetics and install rg on ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 
 create_symlinks() {
-    echo "Creating symlinks..."
+    echo "creating symlinks..."
     stuff=(".aliases .scripts .zshrc .zshenv .bash_profile \
         .bashrc .conkyrc .functions .tmux.conf \
         .Xresources.d .xsessionrc .spacemacs")
@@ -19,8 +19,8 @@ create_symlinks() {
     done
 }
 
-install_apt() {
-    echo "Checking apt packages..."
+install_nix() {
+    echo "checking apt packages..."
     apt_dep=(zsh-antigen build-essential zsh emacs tmux vim tig silversearcher-ag xsel)
 
     if dpkg -l ubuntu-desktop > /dev/null 2>&1; then
@@ -29,19 +29,19 @@ install_apt() {
 
     missing=($(comm -23 <(for i in "${apt_dep[@]}"; do echo $i; done|sort) <(dpkg -l| awk '/^i/{print $2}'|sort)))
     if [ -n "$missing" ]; then
-        echo "Missing apt packages:" "${missing[@]}"
+        echo "missing apt packages:" "${missing[@]}"
         sudo apt-get update
         sudo apt-get install -y "${missing[@]}"
     fi
 
     if ! which nvim 2>&1 > /dev/null; then
-        echo "Installing neovim..."
+        echo "installing neovim..."
         sudo add-apt-repository ppa:neovim-ppa/unstable
         sudo apt-get update
         sudo apt-get install neovim
         sudo apt-get install python-dev python-pip python3-dev python3-pip
         sudo pip3 install neovim
-        # Use neovim as the default for all things vim
+        # use neovim as the default for all things vim
         sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
         sudo update-alternatives --config vi
         sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
@@ -49,16 +49,22 @@ install_apt() {
         sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
         sudo update-alternatives --config editor
     fi
+
+    if which rg 2>&1 > /dev/null; then
+        echo "installing ripgrep..."
+        curl -ssf https://static.rust-lang.org/rustup.sh | sh
+        cargo install ripgrep
+    fi
 }
 
 install_mac() {
     if which rg 2>&1 > /dev/null; then
-        echo "Installing ripgrep..."
+        echo "installing ripgrep..."
         brew install ripgrep
     fi
 
     if which nvim 2>&1 > /dev/null; then
-        echo "Installing neovim..."
+        echo "installing neovim..."
         brew install neovim
     fi
 }
@@ -66,13 +72,13 @@ install_mac() {
 install_packages() {
 
     if which dpkg 2>&1 > /dev/null; then
-        install_apt
+        install_nix
     else
         install_mac
     fi
 
     if ! which fasd 2>&1 > /dev/null; then
-        echo "Installing FASD..."
+        echo "installing fasd..."
         rm -rf /tmp/fasd
         git clone https://github.com/clvv/fasd.git /tmp/fasd
         cd /tmp/fasd
@@ -80,12 +86,13 @@ install_packages() {
     fi
 
     if ! which fzf 2>&1 > /dev/null; then
-        echo "Installing fzf..."
+        echo "installing fzf..."
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
         ~/.fzf/install
     fi
 
     if [ ! -d ~/.emacs.d ]; then
+        echo "installing spacemacs..."
         git clone git@github.com:syl20bnr/spacemacs.git ~/.emacs.d -b develop
     fi
 }
@@ -95,7 +102,7 @@ case $1 in
         :
         ;;
     *)
-        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+        read -p "this may overwrite existing files in your home directory. are you sure? (y/n) " -n 1
         echo
         if [[ ! $REPLY =~ ^[Yy]$ ]]; then
             exit 1
@@ -105,4 +112,4 @@ esac
 
 create_symlinks
 install_packages
-echo "Done!"
+echo "done!"
